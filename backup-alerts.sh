80a9a97a6176e0ba6d11861e7ed42871675be42a
Michael Grace <56653532+michael-grace@users.noreply.github.com>
2023-05-26 16:59:58 +0100

    bash momento

--- backup-alerts.sh
+++ backup-alerts.sh
@@ -51,6 +51,7 @@ daily_snapshot_exist () {
 
 weekly_running_check () {
     [[ $(date +'%w') == 3 -a ! -f $tmp_file ]] && alert "No problems, just to say, I'm still checking for you :)"
+    return 0
 }
 
 for dataset in $musicstore $filestore $database $server_backup; do
